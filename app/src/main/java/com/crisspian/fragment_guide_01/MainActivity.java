d38526f63b1fd78947a26fe8df2c82392ec809de
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-08-12 23:05:03 -0400

    Aparece Picachu

--- app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
+++ app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
@@ -1,15 +1,41 @@
 package com.crisspian.fragment_guide_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.crisspian.fragment_guide_01.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate((getLayoutInflater()));
+        setContentView(binding.getRoot());
+        binding.boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarFragmento();
+            }
+        });
     }
+    private void mostrarFragmento(){
+        //Genero la instancia del fragmento gracis alñ factoory metodo
+        FirstFragment firstFragment = FirstFragment.newInstance("","");
+        //obtener instancias del fragment manager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //Obtener e instanciamos la una transaccion
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+        fragmentTransaction.add(R.id.fragment,firstFragment).addToBackStack(null).commit();
+
+}
 }
