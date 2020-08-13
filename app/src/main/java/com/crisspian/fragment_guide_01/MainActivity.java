0971c047c56afa1c05ee2f141518c589800bb100
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-08-12 23:35:17 -0400

    Cambio a set on click en el boton

--- app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
+++ app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
 import com.crisspian.fragment_guide_01.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     private ActivityMainBinding binding;
 
     @Override
@@ -20,14 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate((getLayoutInflater()));
         setContentView(binding.getRoot());
-        binding.boton1.setOnClickListener(new View.OnClickListener() {
+    }
+     /*   binding.boton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mostrarFragmento();
             }
-        });
-    }
-    private void mostrarFragmento(){
+        });*/
+
+    public void mostrarFragmento(View view){
         //Genero la instancia del fragmento gracis alñ factoory metodo
         FirstFragment firstFragment = FirstFragment.newInstance("","");
         //obtener instancias del fragment manager
